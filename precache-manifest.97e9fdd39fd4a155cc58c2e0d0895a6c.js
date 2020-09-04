e87cff61d4dad94ce4a59ed3ea447d9fe9b0e5a4
flexboks <fredrik@flexboks.com>
2020-09-04 09:53:48 +0200

    First

---
+++ precache-manifest.97e9fdd39fd4a155cc58c2e0d0895a6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23da15e4cc46685aac1b164168d0d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d98ab78c140d87a460",
+    "url": "/static/css/main.a29c2b96.chunk.css"
+  },
+  {
+    "revision": "c92100219c12387c76cb",
+    "url": "/static/js/2.100971c5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.100971c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d98ab78c140d87a460",
+    "url": "/static/js/main.ae498b63.chunk.js"
+  },
+  {
+    "revision": "be570755d094db4851e9",
+    "url": "/static/js/runtime-main.0e64d81d.js"
+  },
+  {
+    "revision": "cec9d78691f659a3259eb3c9b3769a52",
+    "url": "/static/media/HSB_sponsorlogo.cec9d786.png"
+  },
+  {
+    "revision": "314bc8080b60abeba2667546b2f70fee",
+    "url": "/static/media/Tiendebytte_frokost_bg.314bc808.jpg"
+  }
+]);
